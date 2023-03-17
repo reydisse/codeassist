7b48ee9d3ed0e552feb7487773992ccbc61f7a7e
reydisse <reydisse@gmail.com>
2023-03-17 13:24:05 +0000

    initial

--- client/script.js
+++ client/script.js
@@ -37,3 +37,44 @@ function genereteUniqueId() {
 
   return `id-${timestamp}-${hexadecimalString}`;
 }
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAi && "ai"}">
+    <div class="chat">
+    <div class="profile">
+    <img src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}" />
+    </div>
+    <div class="message" id=${uniqueId}>${value}</div>
+    </div>
+    </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset();
+
+  //bot's chatstripe
+  const uniqueId = genereteUniqueId();
+  chatContainer.innerHTML += chatStripe(true, "", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
